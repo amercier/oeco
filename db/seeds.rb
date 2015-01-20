7333f80398c1e763c6fc576934722facff541c82
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2015-01-19 17:31:22 -0800

    Removed news seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,3 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 News.delete_all
-News.create(order: 1, url: 'bureaux-l'      , title: 'Bureaux L, bureaux pour des agriculteurs à Liposthey (Landes)'                    , summary: 'Prix de la Première Oeuvre Le Moniteur 2011 - Vanessa Larrère Architecte')
-News.create(order: 2, url: 'creche-amouroux', title: 'Crèche Amouroux, concours d\'un centre petite enfance et multi-accueil à Toulouse', summary: 'Projet Lauréat')
-News.create(order: 3, url: 'logements-g'    , title: 'Logements G, réhabilitation & extension à Labouheyre (Landes)'                    , summary: 'Projet Livré')
-News.create(order: 4, url: 'maison-du-golf' , title: 'Maison du golf, Biscarrosse (Landes)'                                             , summary: 'Permis Accepté')
