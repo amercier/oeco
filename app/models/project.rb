a2d1338667b1dba951e814071c292a3817740e9b
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-02-16 13:20:49 -0800

    Replaced Project#to_param by view-specific solution

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,4 @@ class Project < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   has_and_belongs_to_many :category
   has_and_belongs_to_many :property
-
-  def to_param
-    return url
-  end
 end
